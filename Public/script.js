6a005a35630777ffb7948984dec2ae791658e400
deepharshsri <deepharshsri.98@gmail.com>
2021-01-04 21:31:29 +0530

    Cart added
    Signed-off-by: deepharshsri <deepharshsri.98@gmail.com>

--- Public/script.js
+++ Public/script.js
@@ -1,8 +1,29 @@
 $(()=>{
-const tel=$('#television')
-
-    tel.click(()=>{
-    
-    
+const root=$('.root')
+const cart=$('#cart')
+const item=$('#item')
+const add=$('#additem')
+   
+    cart.click(()=>{
+        $.get('/',
+     ()=>{
+        // window.alert("Cart Successfully")
+        window.location.replace('http://localhost:2222/cart.html')
+     })
+        console.log("Clicked")
     })
+   add.click(()=>{
+   var brand=$(add).parent('div').parent('div').parent('div').find('h6').text()
+   var price=$(add).parent('div').parent('div').parent('div').find('p').text()
+  
+   root.text(brand)
+   console.log(brand+" "+price)
+   })
+   $(function(){
+
+      $("a").click(function(){
+        $(this).after("<img src='./Images/sr.png' alt='loading' />").fadeIn();  
+      });
+    
+    });
 })
